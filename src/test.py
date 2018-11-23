9612cb9f337c434b0b8dce5c6dc7668b3a833a1e
kristina <44196228+NCC-1701-B@users.noreply.github.com>
2018-11-23 19:05:29 +0700

    source

---
+++ src/test.py
@@ -0,0 +1,25 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+
+
+img_width, img_height = 100, 100
+test_dir = '/home/kris/Рабочий стол/Dataset/Train/test/'
+nb_test_samples = 50
+model=load_model('/home/kris/BarcodesNeural/detect.model')
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+
+test_generator=test_datagen.flow_from_directory(
+        test_dir,
+        target_size=(img_width, img_height),
+        batch_size=10,
+        class_mode='binary')
+
+
+print("model evaluate")
+scores=model.evaluate_generator(test_generator, nb_test_samples)
+print("Точность на тестовых данных: %.2f%%"%(scores[1]*100))
+a=model.predict_generator(test_generator, nb_test_samples)
+print("Prediction: ", a)
+
+
---
+++ src/train.py
@@ -0,0 +1,61 @@
+from PIL import Image
+import numpy as np
+import os
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+
+#paths to find train and test data
+img_width, img_height = 100, 100
+batch_size=10
+train_dir = '/home/kris/Рабочий стол/Dataset/Train/train/'
+val_dir = '/home/kris/Рабочий стол/Dataset/Train/val/'
+test_dir = '/home/kris/Рабочий стол/Dataset/Train/test/'
+nb_train_samples = 100
+nb_val_samples = 500
+nb_test_samples=50
+epochs = 10
+
+
+model = Sequential()
+model.add(Conv2D(8,(3,3),input_shape=(100,100,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2))
+model.add(Conv2D(12,(3,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2))
+model.add(Conv2D(16,(3,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(units=128,activation='relu'))
+model.add(Dense(units=1,activation='sigmoid'))
+
+#model.add(Dropout(0.5))
+
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+
+datagen=ImageDataGenerator(rescale=1./255)
+train_generator=datagen.flow_from_directory(train_dir,
+                                            target_size=(img_width, img_height),
+                                            batch_size=batch_size, class_mode='binary')
+val_generator=datagen.flow_from_directory(val_dir,
+                                          target_size=(img_width,img_height),
+                                          batch_size=batch_size, class_mode='binary')
+test_generator=datagen.flow_from_directory(test_dir,
+                                           target_size=(img_width, img_height),
+                                           batch_size=batch_size, class_mode='binary')
+testgen=ImageDataGenerator(rescale=1./255)
+
+model.fit_generator(train_generator,steps_per_epoch=nb_train_samples//batch_size,epochs=epochs,
+                    validation_data=val_generator,
+                    validation_steps=nb_val_samples//batch_size)
+scores=model.evaluate_generator(test_generator, nb_test_samples//batch_size)
+print("Точность на тестовых данных: %.2f%%"%(scores[1]*100))
+
+model.save('/home/kris/BarcodesNeural/detect.model')
+print('Model saved')
+
+model.save_weights("/home/kris/BarcodesNeural/weights.h5")
+print("Weights saved")
