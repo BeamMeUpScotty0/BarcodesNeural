8081237e45813daaa34767effb261f77c538642b
FlosTaurus <nov.kri@yandex.ru>
2018-11-23 19:12:19 +0700

    source

--- src/test.py
+++ src/test.py
@@ -1,12 +1,12 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 
-import cv2
+
 img_width, img_height = 100, 100
 test_dir = '/home/kris/Рабочий стол/Dataset/Train/test/'
 nb_test_samples = 50
 model=load_model('/home/kris/BarcodesNeural/detect.model')
-test_datagen=ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 
 test_generator=test_datagen.flow_from_directory(
@@ -23,13 +23,3 @@ a=model.predict_generator(test_generator, nb_test_samples)
 print("Prediction: ", a)
 
 
-
-    testgen = ImageDataGenerator(rescale=1. / 255)
-    now_generator = testgen.flow_from_directory(sys.argv[3], target_size=(150, 150), batch_size=1,
-                                                class_mode='binary')
-    a=int(1-(model.predict_generator(now_generator, 1//1)))
-    print(a)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-	        break
-cap.release()
-cv2.destroyAllWindows()
--- src/train.py
+++ src/train.py
@@ -5,51 +5,19 @@ from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
+from keras.preprocessing.image import ImageDataGenerator
 
 #paths to find train and test data
-train_dir = '/home/kris/Рабочий стол/Dataset/Train/Codes_small/'
-train_non_dir = '/home/kris/Рабочий стол/Dataset/Train/No_Small/'
-test_dir = '/home/kris/Рабочий стол/Dataset/Test/1/'
-test_non_dir = '/home/kris/Рабочий стол/Dataset/Test/2/'
-
-
-
-#get data set count
-train_sample = len(os.listdir(train_dir))
-train_non_sample =len(os.listdir(train_non_dir))
-test_sample = len(os.listdir(test_dir))
-test_non_sample = len(os.listdir(test_non_dir))
-
-print("Train Set samples- with codes-"+str(train_sample)+" Without codes-"+str(train_non_sample))
-print("Test Set samples- with codes-"+str(test_sample)+" Without codes-"+str(test_non_sample))
-
-train_x_data_set=np.zeros([train_sample+train_non_sample, 100, 100, 3])
-
-print("shape of training data set: "+ str(train_x_data_set.shape))
-
-    #load images containing B in train_x_data_set matrix
-for index,filename in enumerate(os.listdir(train_dir)):
-    img = Image.open(train_dir+'/'+filename)
-    img = img.resize((100,100),Image.ANTIALIAS)
-    im = np.array(img)
-    train_x_data_set[index, :, :, :]=im
-
-
-    #load images that does not contain B in train_x_data_set matrix
-for index,filename in enumerate(os.listdir(train_non_dir)):
-    img = Image.open(train_non_dir+filename)
-    img = img.resize((100,100),Image.ANTIALIAS)
-    im=np.array(img)
-    train_x_data_set[train_sample+index,:,:,:]=im
-
-
-
-train_x_data_set = train_x_data_set/255
-train_y_data_set=np.array([])
-train_y_data_set=np.append(np.append(train_y_data_set,[1]*train_sample),[0]*train_non_sample)
-
-print("shape of train label:"+str(train_y_data_set.shape))
+img_width, img_height = 100, 100
+batch_size=10
+train_dir = '/home/kris/Рабочий стол/Dataset/Train/train/'
+val_dir = '/home/kris/Рабочий стол/Dataset/Train/val/'
+test_dir = '/home/kris/Рабочий стол/Dataset/Train/test/'
+nb_train_samples = 1000
+nb_val_samples = 500
+nb_test_samples=50
+epochs = 10
 
 
 model = Sequential()
@@ -62,59 +30,32 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dense(units=128,activation='relu'))
 model.add(Dense(units=1,activation='sigmoid'))
-model.add(Dropout(0.2))
+
+model.add(Dropout(0.5))
 
 model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
-model.fit(train_x_data_set,train_y_data_set,epochs=10)
-
-test_x_data_set=np.zeros([test_sample+test_non_sample,100,100,3])
-test_file_list = []
 
 
-for index,filename in enumerate(os.listdir(test_dir)):
-    img = Image.open(test_dir+filename)
-    test_file_list.append(filename)
-    img = img.resize((100,100),Image.ANTIALIAS)
-    im = np.array(img)
-    test_x_data_set[index,:,:,:]=im
+datagen=ImageDataGenerator(rescale=1./255)
+train_generator=datagen.flow_from_directory(train_dir,
+                                            target_size=(img_width, img_height),
+                                            batch_size=batch_size, class_mode='binary')
+val_generator=datagen.flow_from_directory(val_dir,
+                                          target_size=(img_width,img_height),
+                                          batch_size=batch_size, class_mode='binary')
+test_generator=datagen.flow_from_directory(test_dir,
+                                           target_size=(img_width, img_height),
+                                           batch_size=batch_size, class_mode='binary')
+testgen=ImageDataGenerator(rescale=1./255)
 
+model.fit_generator(train_generator,steps_per_epoch=nb_train_samples//batch_size,epochs=epochs,
+                    validation_data=val_generator,
+                    validation_steps=nb_val_samples//batch_size)
+scores=model.evaluate_generator(test_generator, nb_test_samples//batch_size)
+print("Точность на тестовых данных: %.2f%%"%(scores[1]*100))
 
-for index,filename in enumerate(os.listdir(test_non_dir)):
-    img = Image.open(test_non_dir+filename)
-    test_file_list.append(filename)
-    img = img.resize((100,100),Image.ANTIALIAS)
-    im = np.array(img)
-    test_x_data_set[test_sample+index,:,:,:]=im
-
-test_x_data_set = test_x_data_set/255
-
-test_y_data_set=np.array([])
-test_y_data_set=np.append(np.append(test_y_data_set,[1]*test_sample),[0]*test_non_sample)
-
-
-
-model.evaluate(test_x_data_set,test_y_data_set)
 model.save('/home/kris/BarcodesNeural/detect.model')
 print('Model saved')
 
-
-
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-
 model.save_weights("/home/kris/BarcodesNeural/weights.h5")
 print("Weights saved")
-
-
-time_predictions=model.predict(test_x_data_set)
-#print("time predictions: ", time_predictions)
-print("Evaluation: ", model.evaluate(test_x_data_set,test_y_data_set))
-for filename,predict in zip(test_file_list,time_predictions):
-    print(filename + "-->" + str(predict))
-    if(predict >= 0.7000):
-        print('There is code')
-    else:
-        print("There is no code")
-
-model.save('/home/kris/BarcodesNeural/B_detect_24_feb.model')
